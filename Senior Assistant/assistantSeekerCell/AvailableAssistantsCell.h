2365b1678c1718d10169a91df4c7d6bd8ff244fa
B Bennett <Brockbennett2324@gmail.com>
2019-04-14 01:11:13 -0400

    created custom cell for Available Assistantspage

---
+++ Senior Assistant/assistantSeekerCell/AvailableAssistantsCell.h
@@ -0,0 +1,19 @@
+//
+//  AvailableAssistantsCell.h
+//  Senior Assistant
+//
+//  Created by chung Ng-a on 4/14/19.
+//  Copyright © 2019 Zelalem Terefe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AvailableAssistantsCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
